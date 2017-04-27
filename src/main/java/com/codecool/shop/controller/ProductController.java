a87cd06421c0ffddeede4e60ef908836db322474
annakonda <csepregicode@gmail.com>
2017-04-27 14:03:07 +0200

    getting close to filter for suppliers
    (cherry picked from commit d21cfa2)

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,11 +2,11 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
 import spark.ModelAndView;
 import spark.Request;
@@ -30,17 +30,17 @@ public class ProductController {
         params.put("categories", categories);
 
 
-        params.put("suppliers", supplierCategoryStore.find(1));
+        params.put("supplier", supplierCategoryStore.find(1));
 
         List<Supplier> suppliers = supplierCategoryStore.getAll();
         params.put("suppliers", suppliers);
 
-        String selectedSupplerName = req.params(":supplerName");
-        if (selectedSupplerName == null) {
-            params.put("suppliers", supplierCategoryStore.getAll());
+        String selectedSupplierName = req.params(":supplierName");
+        if (selectedSupplierName == null) {
+            params.put("products", productDataStore.getAll());
         } else {
-            Supplier selectedCategory = productCategoryDataStore.find(selectedSupplerName);
-            params.put("suppliers", productDataStore.getBy(selectedCategory));
+            Supplier selectedSupplier = supplierCategoryStore.find(selectedSupplierName);
+            params.put("products", productDataStore.getBy(selectedSupplier));
         }
 
 
