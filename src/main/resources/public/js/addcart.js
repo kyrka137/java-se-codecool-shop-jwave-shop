b213c708fa3ab7f2da213d848ae2ca9e17193556
annakonda <csepregicode@gmail.com>
2017-05-08 10:35:56 +0200

    add to cart

--- src/main/resources/public/js/addcart.js
+++ src/main/resources/public/js/addcart.js
@@ -2,16 +2,18 @@
  * Created by gabor on 2017.04.27..
  */
 
-$( document ).ready(function() {
+$(document).ready(function() {
+    var titleList = [];
+    $(".btn-success").on("click", function() {
+        var title = $(this).closest(".caption").find("h4").text();
+        var element = $.inArray(title, titleList);
+        if(element === -1) {
+            titleList.push(title);
+            $(".modal-body").prepend("<div>" + title + "</div>");
+        }
 
-    function addToCart(id) {
-        $.ajax({
-            method: 'POST',
-            url: '/addtocart',
-            data: {'id': id},
-            success: function () {
-                $("#").text(parseInt($("#").html()) + 1);
-            }
-        });
-    }
-}
+        console.log(titleList);
+
+        console.log(title);
+    });
+});
