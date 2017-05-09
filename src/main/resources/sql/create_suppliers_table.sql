246cdad6742db64445468fa39ce536a215ad0f45
kyrka137 <munyorelated@gmail.com>
2017-05-09 11:32:12 +0200

    Gabor & (?) had a bugfix on .sql file, i try to git it :)

--- src/main/resources/sql/create_suppliers_table.sql
+++ src/main/resources/sql/create_suppliers_table.sql
@@ -1,25 +1,26 @@
+DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE suppliers
 (
-id varchar(36) PRIMARY KEY,
-name varchar(40)
+  id SERIAL PRIMARY KEY,
+  name varchar
 );
 
-
-DROP TABLE IF EXISTS products;
+CREATE TABLE categories
+(
+  id SERIAL PRIMARY KEY,
+  name varchar
+);
 
 CREATE TABLE products
 (
-id varchar(36) PRIMARY KEY,
-name varchar(40)
-supplier integer references (suppliers.id)
-);
-
-DROP TABLE IF EXISTS productsCategory;
-
-CREATE TABLE productCategory
-(
-id varchar(36) PRIMARY KEY,
-name varchar(40)
+  id SERIAL PRIMARY KEY,
+  name varchar,
+  defaultPrice float,
+  currencyString varchar,
+  description varchar,
+  productCategory INTEGER REFERENCES categories(id),
+  supplier INTEGER REFERENCES suppliers(id)
 );
