706ede09761aee4674c33034653e675cafaa2ba4
kyrka137 <munyorelated@gmail.com>
2017-05-11 22:26:39 +0200

    ProductCategoryDaoJdbcTest implemented :)

---
+++ src/tests/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbcTest.java
@@ -0,0 +1,58 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.dao.implementation.ProductCategoryDaoJdbc;
+import com.codecool.shop.model.ProductCategory;
+import jdk.nashorn.internal.objects.annotations.Optimistic;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Created by kyrka on 2017.05.11..
+ */
+public class ProductCategoryDaoJdbcTest {
+
+    @Test
+    public void getALL() {
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoJdbc();
+        assertNotNull(productCategoryDao.getAll());
+    }
+
+
+    @Test
+    public void add() {
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoJdbc();
+        ProductCategory productcategory = new ProductCategory(137, "productcategoryname", "", "");
+        productCategoryDao.add(productcategory);
+        assertNotNull(productCategoryDao.find(137));
+    }
+
+    @Test
+    public void find_string() {
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoJdbc();
+        ProductCategory productcategory = new ProductCategory(137, "productcategoryname", "", "");
+        productCategoryDao.add(productcategory);
+        assertEquals(137, productCategoryDao.find("productcategoryname").getId());
+    }
+
+
+    @Optimistic
+    @Test
+    public void find_int() {
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoJdbc();
+        ProductCategory productcategory = new ProductCategory(137, "productcategoryname", "", "");
+        productCategoryDao.add(productcategory);
+        assertEquals("productcategoryname", productCategoryDao.find(137).getName());
+    }
+
+    @Test
+    public void remove() {
+        ProductCategoryDao productCategoryDao = new ProductCategoryDaoJdbc();
+        ProductCategory productcategory = new ProductCategory(731, "productcategoryname", "", "");
+        productCategoryDao.add(productcategory);
+        productCategoryDao.remove(731);
+        ProductCategory productCategory2_found = productCategoryDao.find(731);
+        assertEquals(null, productCategory2_found);
+    }
+}
